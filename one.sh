ad9acf464264bdca06ca905e460e3dd0bc8aedd9
harshada-1591 <68768054+harshada-1591@users.noreply.github.com>
2024-03-15 23:32:10 +0530

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-read -p "enter the first no : "a
-read -p "enter the second no : "b
+read -p "enter the first no : " a
+read -p "enter the second no : " b
 if [ $a -gt $b ] 
 then
-echo "the first no $a is greater"
+echo "the first no  $a  is greater"
 else
 echo "the second no $b is greater"
 fi
