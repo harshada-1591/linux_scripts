6f58a93a2b77f0cf3c568e709ddd7463158189a3
harshada-1591 <68768054+harshada-1591@users.noreply.github.com>
2024-03-15 23:37:36 +0530

    Update three.sh

--- three.sh
+++ three.sh
@@ -2,10 +2,10 @@
 read -p "Enter the 1st number :"a
 read -p "Enter the 2nd number :" b
 read -p "Enter the 3rd number :"c
-if [ $a-gt $b ] && [ $a -gt $c ]
+if [ $a -gt $b ] && [ $a -gt $c ]
 then
 echo "Maximum number is :" $a
-elif [ $b -gt $a ] && [ $b-gt $c ]
+elif [ $b -gt $a ] && [ $b -gt $c ]
 then
 echo "Maximum number is " $b
 else 
